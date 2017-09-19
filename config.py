7cdf1f4c9db60257828114d18d2f81f882415ae1
shogun <vityamoshennik@gmail.com>
2017-09-19 22:30:06 +0300

    1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TToken = '375703496:AAHYGcnzgmOG6NZQ_AKpZPougBSYzBBHU4U'
-vkToken = '34ed023f34ed023f34ed023fca34bd0c6e334ed34ed023f6d2c34a1973477e3d481744b'
+TToken = '...'
+vkToken = '...'
 
-vkPublic = 'hynta_atmta'
-TChannel = '@whatameme'
+vkPublic = 'public_name'
+TChannel = '@channel_name'
